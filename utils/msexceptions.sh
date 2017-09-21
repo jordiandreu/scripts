4cc489aa09c248cd8ed023b6c184720c91267a02
Jordi Andreu <jandreu@cells.es>
2017-09-21 11:57:24 +0200

    [Add] Update script for macroserver analysis

--- utils/msexceptions.sh
+++ utils/msexceptions.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
+echo "================================================================="
+echo "             Starting  MacroServer log analysis"
+echo "================================================================="
+echo "Log file: $1"
+echo "MacroServer stats since $(tar xOfz $1 |head -n1|cut -c25-47)";
+echo "MacroServer Restarts $(zcat $1 | grep -a "Ready to accept request" | cut -c 49-92| wc -l;)"
+echo;
+echo "runMacro Rankings";
+echo "================================================================="
+zcat $1 |grep -a "runMacro Macro"|cut -c 101-|cut -d \( -f1| sort|uniq -c|sort -nr ;
+echo;
+echo "MacroServer Exceptions list";
+echo "================================================================="
+zcat $1 |grep -a ENDEX |grep -v StopException | cut -c 77- | sort | uniq -c |sort -nr;
+echo;
+
 top=5
 alines=4
 blines=4
 
-echo "Searching top ${top} macros which end with Macroserver Exception:"
+# 1) Details for macros with the highest number of exceptions.
+echo "Searching top ${top} macros ended with Macroserver Exception:"
 echo "================================================================="
-# 1) Search for macros that has failed:
-grep '(MacroServerException) runMacro' ${1} |cut -c 85-| sort | uniq -c | sort -nr | head -${top}
+zcat $1 | grep -a '(MacroServerException) runMacro' |cut -c 85-| sort | uniq -c | sort -nr | head -${top}
 # create a list with all macro names:
-macros_list=$(grep '(MacroServerException) runMacro' ${1} |cut -c 85-| sort | uniq -c | sort -nr | head -${top} | cut -c 40- |xargs)
-
+macros_list=$(zcat $1 | grep -a '(MacroServerException) runMacro' |cut -c 85-| sort | uniq -c | sort -nr | head -${top} | cut -c 40- |xargs)
+echo;
 
 # 2) Get macro dependencies. For each macro, search if the error was raised to a secondary macro
 for macro in $macros_list
 do
 echo "Searching dependencies for macro " $macro:
 echo "=========================================================="
-grep -B $blines -A $alines -w '(MacroServerException) runMacro '${macro} ${1} |grep ENDEX | cut -c 85- | sort | uniq -c | sort -nr
+zcat $1 | grep -a -B $blines -A $alines -w '(MacroServerException) runMacro '${macro} |grep ENDEX | cut -c 85- | sort | uniq -c | sort -nr
 echo " "
 done
 
@@ -27,7 +43,7 @@ for macro in $macros_list
 do
 echo "Searching error descriptions for macro " $macro:
 echo "=========================================================="
-grep -A 40 -w '(MacroServerException) runMacro '${macro} ${1} |grep 'desc =' | sort | uniq -c | sort -nr
+zcat $1 | grep -a -A 40 -w '(MacroServerException) runMacro '${macro} |grep 'desc =' | sort | uniq -c | sort -nr
 echo " "
 done
 
